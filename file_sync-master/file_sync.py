9ec97a0844a0f19f0848edb184f4c7451b954db8
ywz978020607 <ywzsunny@buaa.edu.cn>
2022-06-19 00:10:21 +0800

    Update.

--- file_sync-master/file_sync.py
+++ file_sync-master/file_sync.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from sqlite3 import Timestamp
 import sys
 import time
 import ntpath
@@ -12,30 +13,33 @@ from subprocess import call
 from shutil import copy
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import threading
 
 # git root path for files to push to remote
 DIR_FOR_GIT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# print(__file__)
-# print(DIR_FOR_GIT)
 
-# files to synchronize
-SYNC_FILE_LIST = []
+# folders to synchronize
+SYNC_FOLDER_LIST = []
 f = open(os.path.join(DIR_FOR_GIT, "file_list.txt"), "r")
 try:
-    SYNC_FILE_LIST = [line.strip().replace('\\','/') for line in f] # if os.path.isfile(line.strip().replace('\\','/'))]
+    SYNC_FOLDER_LIST = [line.strip().replace('\\','/') for line in f] # if os.path.isfile(line.strip().replace('\\','/'))]
 except Exception as e:
     raise e
 finally:
     f.close()
 
 class FileChangeHandler(FileSystemEventHandler):
-    # def on_modified(self, event):
+    timer = None
     def on_any_event(self, event):
-        print(event.event_type, event.src_path)
-        print("once")
-        src_path = event.src_path.replace('\\','/')
-        # if src_path in SYNC_FILE_LIST:
-        for SYNC_PATH in SYNC_FILE_LIST:
+        if self.timer:
+            self.timer.cancel()
+        self.timer = threading.Timer(0.2, self.checkSnapshot) #延后
+        self.last_event = event
+        self.timer.start()
+    def checkSnapshot(self):
+        print(self.last_event.event_type, self.last_event.src_path)
+        src_path = self.last_event.src_path.replace('\\','/')
+        for SYNC_PATH in SYNC_FOLDER_LIST:
             if SYNC_PATH in src_path:
                 os.chdir(SYNC_PATH)
                 git_add_cmd = "git add -A"
@@ -57,7 +61,7 @@ if __name__ == "__main__":
     observer = Observer()
     event_handler = FileChangeHandler()
 
-    for file_path in SYNC_FILE_LIST:
+    for file_path in SYNC_FOLDER_LIST:
         observer.schedule(event_handler, path=os.path.dirname(os.path.realpath(file_path)), recursive=True)
 
     observer.start()
--- watch.py
+++ watch.py
@@ -1,21 +1,60 @@
 # demo 监控文件夹下的所有变动
-import time
+
+#-*- coding: utf-8 -*-
+
+import os, threading
 from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
- 
- 
-class MyHandler(FileSystemEventHandler):
+from watchdog.events import *
+from watchdog.utils.dirsnapshot import DirectorySnapshot, DirectorySnapshotDiff
+
+class FileEventHandler(FileSystemEventHandler):
+    def __init__(self, aim_path):
+        FileSystemEventHandler.__init__(self)
+        self.aim_path = aim_path
+        self.timer = None
+        self.snapshot = DirectorySnapshot(self.core.proj_path)
+
     def on_any_event(self, event):
-        print(event.event_type, event.src_path)
- 
- 
-event_handler = MyHandler()
-observer = Observer()
-observer.schedule(event_handler, path='.', recursive=False)
-observer.start()
-try:
-    while True:
-        time.sleep(0.1)
-except KeyboardInterrupt:
-    observer.stop()
-observer.join()
+        if self.timer:
+            self.timer.cancel()
+        self.timer = threading.Timer(0.2, self.checkSnapshot)
+        self.timer.start()
+
+    def checkSnapshot(self):
+        snapshot = DirectorySnapshot(self.aim_path)
+        diff = DirectorySnapshotDiff(self.snapshot, snapshot)
+        self.snapshot = snapshot
+        self.timer = None
+        #下面是应处理的各种事项
+        print("files_created:", diff.files_created)
+        print("files_deleted:", diff.files_deleted)
+        print("files_modified:", diff.files_modified)
+        print("files_moved:", diff.files_moved)
+        print("dirs_modified:", diff.dirs_modified)
+        print("dirs_moved:", diff.dirs_moved)
+        print("dirs_deleted:", diff.dirs_deleted)
+        print("dirs_created:", diff.dirs_created)
+        # 接下来就是你想干的啥就干点啥，或者该干点啥就干点啥
+        pass
+
+class DirMonitor(object):
+    """文件夹监视类"""
+
+    def __init__(self, aim_path):
+        """构造函数"""
+        self.aim_path= aim_path
+        self.observer = Observer()
+
+    def start(self):
+        """启动"""
+        event_handler = FileEventHandler(self.aim_path)
+        self.observer.schedule(event_handler, self.aim_path, True)
+        self.observer.start()
+
+    def stop(self):
+        """停止"""
+        self.observer.stop()
+
+if __name__ == "__main__":
+    monitor = DirMonitor(r"C:\Users\W\Desktop\folder\gitfolder")
+    monitor.start()
